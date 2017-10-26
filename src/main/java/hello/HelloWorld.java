33b6e2c796dab3ec8036005ef041579549633d68
tejas46 <tejas@platformx.tech>
2017-10-26 12:28:27 +0530

    apply war plugin to gradle

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -10,6 +10,6 @@ public class HelloWorld {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
 
-    System.out.println("Triggered Test and Production");
+    System.out.println("Final Deployment stage");
   }
 }
